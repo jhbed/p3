4b995db6d6f824f660ff6cbfe984211dc0971b32
jhbed <jakehbedard@gmail.com>
2018-04-01 13:52:33 -0700

    2

---
+++ p3helper.c
@@ -0,0 +1,55 @@
+/* p3helper.c
+ * Program 3 assignment
+ * CS570
+ * SDSU
+ * Spring 2018
+ *
+ * This is the ONLY file you are allowed to change. (In fact, the other
+ * files should be symbolic links to
+ *   ~cs570/Three/p3main.c
+ *   ~cs570/Three/p3robot.c
+ *   ~cs570/Three/p3.h
+ *   ~cs570/Three/makefile
+ *   ~cs570/Three/CHK.h    )
+ * The file ~cs570/Three/createlinks is a script that will create these for you.
+ */
+#include "p3.h"
+
+/* You may put declarations/definitions here.
+ * In particular, you will probably want access to information
+ * about the job (for details see the assignment and the documentation
+ * in p3robot.c):
+ */
+
+/*JAKE DECLARATIONS*/
+int count;
+int fd;
+/*END JAKE DECLARATIONS*/
+extern int nrRobots;
+extern int seed;
+extern int width;
+extern int quota;
+
+/* General documentation for the following functions is in p3.h
+ * Here you supply the code, and internal documentation:
+ */
+void initStudentStuff(void) {
+
+	/*BEGIN MY CODE, MAY NEED TO DELETE ALL OF THIS*/
+	CHK(fd = open("countfile", O_RDRW | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR))
+	/*END MY CODE */
+}
+
+/* In this braindamaged version of placeWidget, the widget builders don't
+ * coordinate at all, and merely print a random pattern. You should replace
+ * this code with something that fully follows the p3 specification.
+ */
+void placeWidget(int n) {
+  printeger(n);
+  printf("N\n");
+  fflush(stdout);
+}
+
+/* If you feel the need to create any additional functions, please
+ * write them below here, with appropriate documentation:
+ */
